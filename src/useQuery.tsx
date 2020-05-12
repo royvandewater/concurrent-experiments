5e172f43c9deaa68ee6635df48d7a135cdba50cd
Roy van de Water <roy.vwater@gmail.com>
2020-05-12 08:27:19 -0700

    Change default population to 10k

--- src/useQuery.tsx
+++ src/useQuery.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const useQuery = () => {
   const query = new URLSearchParams(window.location.search)
 
-  const [populationSizePower, setPopulationSizePower] = React.useState(parseInt(query.get('psp') || '2'))
+  const [populationSizePower, setPopulationSizePower] = React.useState(parseInt(query.get('populationPower') || '4'))
   const [e1a, setE1a] = React.useState(parseInt(query.get('e1a') || '50'))
   const [e1b, setE1b] = React.useState(parseInt(query.get('e1b') || '50'))
   const [e2a, setE2a] = React.useState(parseInt(query.get('e2a') || '50'))
